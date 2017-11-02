7dbe0134128aed6e16954367b6b3a808c5522cb7
ndiaga <ndiaga@192.168.2.25>
2017-11-01 20:47:39 -0400

    test

--- microbank/models/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package microbank.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long>{
-
-}
--- microbank/models/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package microbank.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContactRepository extends CrudRepository<Contact, Long>{
-
-}
